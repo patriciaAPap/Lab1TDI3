3c095705aa5020f608751cc08f6c61f031563499
patricia.achim <patricia.achim@UCMS-PC80>
2023-09-19 00:07:15 +0300

    Add project files.

---
+++ Lab1TDI3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab1TDI3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine ("Hello abc World!");
+
+            Console.ReadKey();
+        }
+    }
+}

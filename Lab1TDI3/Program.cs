af771146bf3be6295db9121f7f66d1884933956f
patricia.achim <patriciaAPucms@gmail.com>
2023-09-19 00:35:52 +0300

    a

--- Lab1TDI3/Program.cs
+++ Lab1TDI3/Program.cs
@@ -9,8 +9,23 @@ namespace Lab1TDI3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine ("Hello abc World!");
+            
+            /*aria patratului*/
 
+            int lungime = 10;
+            lungime = lungime * 3;
+
+            int latime = 11;
+
+            int aria = lungime * latime;
+
+            Console.WriteLine("Aria dreptunghiului este "+aria);
+
+
+
+
+
+            /*l-am adaugat deoarece fara el, mi se inchide automat debugul fara sa vad ce modificari am facut*/
             Console.ReadKey();
         }
     }
